5eaf9ce2de421da2d2727abba51eebd79a7f1feb
WelliWillers <wellington.willers@gmail.com>
2022-10-25 10:35:03 -0300

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { Container } from './styles';
+
+export function Home() {
+  return (
+    <Container>
+        <Text> Open up App.jsawdawd to start working on your app!</Text>
+    </Container>
+  );
+}
